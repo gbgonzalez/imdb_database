105e4c12210b01581a72c23b7f6c627677b5792e
Guillermo Blanco <guillermo@guillermoblanco.es>
2022-02-28 09:15:33 +0100

    Adds clean code

--- app/db/connect.py
+++ app/db/connect.py
@@ -4,10 +4,9 @@ from sqlalchemy import create_engine
 import json
 import os
 
-def database_connect(target: str = "colorectal"):
+def database_connect():
     if target == "imdb":
         engine = create_engine(
-            #"mysql+pymysql://tweetUser:tweetPass@192.168.200.134:3306/tweet_corona?charset=utf8mb4"
             "mysql+pymysql://imdbUser:imdbPass@localhost:3306/imdb?charset=utf8mb4"
         )
     Session = sessionmaker(bind=engine)
